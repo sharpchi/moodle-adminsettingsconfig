b3a71c373a3a3bf155b69948a0487e417b52a244
Mark Sharp <m.sharp@chi.ac.uk>
2018-03-19 15:42:10 +0000

    intial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# moodle-local_adminsettingsconfig
+
+This is local plugin that adds a new setting type for your plugins. Currently, this is plugin just adds a JSON setting type. The plugin ensures that the text inserted into the textarea is valid JSON, and throws an error if it isn't.
+
+If you're interested in adding some more setting types just do a pull request on the git repo.
+
+This plugin has no UI of its own, it's primarily aimed at developers who will add this plugin as a dependency in their own plugins.
+
+## Installing
+1.  Drop code into /local/adminsettingsconfig
+2.  Go to Site administration -> Notifications to install
+3.  Add
+```
+$plugin->dependencies = ['local_adminsettingsconfig' => 2018031900]```
+to *your* plugin's version.php file.
+4.  Include a setting in *your* plugin's settings.php file.
+```
+$setting = new \local_adminsettingsconfig\admin_setting_configjson($name, $title, $description, $default);
+$setting->set_updatedcallback('theme_reset_all_caches');
+$page->add($setting);
+```
+5. No need to use require_once or include_once.
---
+++ classes/admin_setting_configjson.php
@@ -0,0 +1,93 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   local_adminsettingsconfig
+ * @author    Mark Sharp <m.sharp@chi.ac.uk>
+ * @copyright 2018 University of Chichester {@link www.chi.ac.uk}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_adminsettingsconfig;
+
+defined('MOODLE_INTERNAL') || die();
+/**
+ * Used to validate a textarea used for json.
+ */
+class admin_setting_configjson extends \admin_setting_configtextarea {
+    private $rows;
+    private $cols;
+
+    /**
+     * @param string $name
+     * @param string $visiblename
+     * @param string $description
+     * @param mixed $defaultsetting string or array
+     * @param mixed $paramtype
+     * @param string $cols The number of columns to make the editor
+     * @param string $rows The number of rows to make the editor
+     */
+    public function __construct($name, $visiblename, $description, $defaultsetting, $paramtype=PARAM_RAW, $cols='60', $rows='8') {
+        $this->rows = $rows;
+        $this->cols = $cols;
+        parent::__construct($name, $visiblename, $description, $defaultsetting, $paramtype);
+    }
+
+    /**
+     * Validate the contents of the textarea as JSON
+     *
+     * @param string $data A JSON string
+     * @return mixed bool true for success or string:error on failure
+     */
+    public function validate($data) {
+        if(!empty($data)) {
+            $validjson = json_decode($data);
+        } else {
+            return true;
+        }
+        if (!$validjson) {
+            return get_string('validatejsonerror', 'local_adminsettingsconfig');
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Returns an XHTML string for the editor
+     *
+     * @param string $data
+     * @param string $query
+     * @return string XHTML string for the editor
+     */
+    public function output_html($data, $query='') {
+        global $OUTPUT;
+        $default = $this->get_defaultsetting();
+        $defaultinfo = $default;
+
+        $context = (object) [
+            'cols' => $this->cols,
+            'rows' => $this->rows,
+            'id' => $this->get_id(),
+            'name' => $this->get_full_name(),
+            'value' => $data,
+            'forceltr' => $this->get_force_ltr(),
+        ];
+        $element = $OUTPUT->render_from_template('core_admin/setting_configtextarea', $context);
+
+        return \format_admin_setting($this, $this->visiblename, $element, $this->description, true, '', $defaultinfo, $query);
+    }
+
+}
