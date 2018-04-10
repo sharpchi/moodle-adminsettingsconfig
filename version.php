ff30d97dd1a1787352a20fd234f4a8189b866781
Mark Sharp <m.sharp@chi.ac.uk>
2018-04-10 12:20:13 +0100

    updating status to stable

--- version.php
+++ version.php
@@ -25,6 +25,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018031900;
 $plugin->requires  = 2014051200; // Moodle 2.7.
-$plugin->maturity  = MATURITY_BETA;
+$plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = 'Version for Moodle 2.7 onwards';
 $plugin->component = 'local_adminsettingsconfig';
